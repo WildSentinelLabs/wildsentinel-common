133b88ceaff704dd84e59c14ede6cb23b60ffcff
Jokicklml <109178169+Jokicklml@users.noreply.github.com>
2025-02-10 14:31:52 -0500

    minor imp in fileformat

--- include/io/file_handling/file_format.h
+++ include/io/file_handling/file_format.h
@@ -1,14 +1,13 @@
 #pragma once
 #include <cstdint>
 #include <string>
+#include <vector>
 
 class FileFormat {
  public:
   virtual ~FileFormat() = default;
   virtual std::string Name() const = 0;
   virtual std::string DefaultMimeType() const = 0;
-  virtual const std::string* MimeTypesValues() const = 0;
-  virtual const std::string* FileExtensionsValues() const = 0;
-  virtual uint8_t MimeTypesSize() const = 0;
-  virtual uint8_t FileExtensionsSize() const = 0;
+  virtual const std::vector<std::string> MimeTypes() const = 0;
+  virtual const std::vector<std::string> FileExtensions() const = 0;
 };
