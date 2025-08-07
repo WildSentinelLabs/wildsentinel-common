a7325700e88da6e307a5621c00798b15002ed571
Jokicklml <109178169+Jokicklml@users.noreply.github.com>
2025-08-07 01:27:36 -0500

    add missing header

--- src/ws/imaging/image_decoder.h
+++ src/ws/imaging/image_decoder.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ws/imaging/image.h"
+#include "ws/imaging/image_context.h"
 #include "ws/imaging/image_format.h"
 #include "ws/io/stream.h"
 #include "ws/logging/ilogger.h"
